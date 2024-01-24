f19caea23a5df62c0757211c43b5226113477003
박수진 <tnwlssla20@naver.com>
2024-01-24 15:58:10 +0900

    add methods

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
 	static List<Integer> computerNum = new ArrayList<>();
 	static List<Integer> userNum = new ArrayList<>();
+	static int ballCount = 0;
+	static int strikeCount = 0;
+	static boolean finish = false;
 	
     public static void main(String[] args) {
         startBaseballGame();
@@ -19,14 +22,13 @@ public class Application {
     
     public static void startBaseballGame() {
     	System.out.println("숫자 야구 게임을 시작합니다.");
-    	System.out.print("숫자를 입력해주세요 : ");
     	
     	computerNum = getComputerNum();
     	userNum = getUserNum();
-    	if (userNum.size() != 3) throw new IllegalArgumentException();
+    	
+    	verifyUserNum();
     	
     	getBaseballGameResult();
-    	
     }
     
     public static List<Integer> getComputerNum() {
@@ -40,6 +42,7 @@ public class Application {
     }
     
     public static List<Integer> getUserNum() {
+    	System.out.print("숫자를 입력해주세요 : ");
     	String userStr = Console.readLine();
     	
     	return Arrays.stream(userStr.split(""))
@@ -47,14 +50,77 @@ public class Application {
     			.boxed().collect(Collectors.toList());
     }
     
-    public static void getBaseballGameResult() {
-    	//정답 아니면 getUserNum() & getBaseballGameResult()
-    	for (int i=0; i<3; i++) {
-    		int targetNum = userNum.get(i);
+    public static void verifyUserNum() {
+    	isLengthThree();
+    	isInDuplicatedNum();
+    }
+    
+    public static void isLengthThree() {
+    	if (userNum.size() != 3) throw new IllegalArgumentException();
+    }
+    
+    public static void isInDuplicatedNum() {
+    	int targetNum = userNum.get(0);
+    	for (int i=1; i<userNum.size(); i++) {
+    		if (targetNum == userNum.get(i)) throw new IllegalArgumentException();
+    		targetNum = userNum.get(i);
     	}
     }
     
-    public static void isSameWithComputerNum() {
+    public static void getBaseballGameResult() {
+    	ballCount = 0;
+    	strikeCount = 0;
+    	for (int i=0; i<3; i++) {
+    		int targetNum = userNum.get(i);
+    		isSameWithComputerNum(targetNum, i);
+    	}
     	
+    	printBaseballGameResult();
+    	
+    	if (finish) {
+    		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    		finish = false;
+    		whetherRestartGame();
+    	} else {
+    		userNum = getUserNum();
+        	verifyUserNum();
+        	
+        	getBaseballGameResult();
+    	}
+    }
+    
+    public static void isSameWithComputerNum(int targetNum, int idx) {
+    	if (computerNum.contains(targetNum) 
+    			&& computerNum.indexOf(targetNum) == idx) {
+    		strikeCount++;
+    	} else if (computerNum.contains(targetNum) 
+    			&&computerNum.indexOf(targetNum) != idx ) {
+    		ballCount++;
+    	} else return;
+    }
+    
+    public static void printBaseballGameResult() {
+    	if (ballCount == 0 && strikeCount == 0) {
+    		System.out.println("낫싱");
+    	} else if (ballCount == 0) {
+    		System.out.println(strikeCount + "스트라이크");
+    	} else if (strikeCount == 0) {
+    		System.out.println(ballCount + "볼");
+    	} else {
+    		System.out.printf("%d볼 %d스트라이크\n", ballCount, strikeCount);
+    	}
+    	
+    	if (strikeCount == 3) finish = true;
+    }
+    
+    public static void whetherRestartGame() {
+    	String restartNum = Console.readLine();
+    	
+    	if (restartNum.equals("1")) {
+    		startBaseballGame();
+    	} else if (restartNum.equals("2")) {
+    		return;
+    	} else throw new IllegalArgumentException();
     }
 }
